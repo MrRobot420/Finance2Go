012da16a34d1c9541a21e0dce8a055a6fc486112
MrRobot420 <karl_maximilian@gmx.net>
2019-05-19 02:00:16 +0200

    Fixed Bug

--- Finance 2 Go/OverviewVC.swift
+++ Finance 2 Go/OverviewVC.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class OverviewVC: UIViewController {
     
+    @IBOutlet weak var accountLabel: UILabel!
+    @IBOutlet weak var saldoLabel: UILabel!
+    @IBOutlet weak var transxLabel: UILabel!
+    
+    @IBAction func accountBtn(_ sender: Any) {
+        
+    }
+    
+    @IBAction func saldoBtn(_ sender: Any) {
+        
+    }
+    
+    @IBAction func transxBtn(_ sender: Any) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
