c6e7dc9b05e111f03d67da981212c5db3720a90e
MrRobot420 <max.karlchen@web.de>
2019-05-29 18:28:55 +0200

    Added possibility to delete Profiles

--- Finance 2 Go/SettingsVC.swift
+++ Finance 2 Go/SettingsVC.swift
@@ -7,13 +7,58 @@
 //
 
 import UIKit
+import CoreData
 
-class SettingsVC: UIViewController {
+class SettingsVC: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var deleteInfo: UILabel!
+    let deletionInformation = "ALL Profiles DELETED!"
+    
+    // Deletes all profiles:
+    func deleteAllData(_ sender: Any) {
+        deleteData("Profile")
+    }
+    
+    @IBAction func showAlert() {
+        let message = "Dies löscht ALLE Profile ‼️"
+        let alert = UIAlertController(title: "Profile löschen?", message: message, preferredStyle: .alert)
+        print("[i] Showing profile-deletion alert")
+        alert.addAction(UIAlertAction(title: "NEIN ❌", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Ja ✅", style: .default, handler: deleteAllData(_:)))
+        self.present(alert, animated: true)
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("\n###########   SETTINGS:   ###########\n")
         // Do any additional setup after loading the view.
     }
     
+    
+    //TO DELETE ALL DATA! (copied from net)
+    func deleteData(_ entity: String) {
+        let context = (UIApplication.shared.delegate as? AppDelegate)!.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: entity)
+        fetchRequest.returnsObjectsAsFaults = false
+        do {
+            let results = try context.fetch(fetchRequest)
+            for object in results {
+                guard let objectData = object as? NSManagedObject else {continue}
+                context.delete(objectData)
+                deleteInfo.text = deletionInformation
+                print("[√] Deleted all profiles!")
+                do {
+                    try context.save()
+                    print("[√] Saved!")
+                } catch {
+                    print("[X] Failed Saving!")
+                }
+            }
+        } catch let error {
+            print("Detele all data in \(entity) error :", error)
+        }
+    }
     
 }
